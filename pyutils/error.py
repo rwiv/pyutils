b3725df3cb7dac3414e3974aa3f19efefec4456e
rwiv <zc798546@gmail.com>
2025-02-19 20:40:46 +0900

    feat: update errors

--- pyutils/error.py
+++ pyutils/error.py
@@ -1,26 +1,51 @@
 import sys
 import traceback
+from dataclasses import dataclass, asdict
 from typing import Any
 
 
-def stacktrace():
+@dataclass
+class ErrorDetails:
+    type: str
+    message: str
+    stacktrace: str
+    
+
+def stacktrace() -> str:
     exc_info = sys.exc_info()
     trace = traceback.format_exception(*exc_info)
     return "".join(trace)
 
 
-# e.g. log.error(*stacktrace_details())
-def stacktrace_details() -> tuple[str, dict[str, Any]]:
+def stacktrace_details() -> ErrorDetails:
     exc_info = sys.exc_info()
     trace = traceback.format_exception(*exc_info)
     head = trace[len(trace) - 1].replace("\n", "")
-    return head, {"stacktrace": "".join(trace)}
+    err_type, message = split_once(head, ":")
+    return ErrorDetails(err_type, message, "".join(trace))
+
+
+def split_once(s: str, delimiter: str = ":"):
+    parts = s.split(delimiter, 1)
+    if len(parts) == 1:
+        return "", s
+    return parts[0].strip(), parts[1].strip()
+
+
+# e.g. log.error(*stacktrace_details())
+def stacktrace_entry() -> tuple[str, dict[str, Any]]:
+    details = stacktrace_details()
+    return details.message, asdict(details)
+
+
+def error_details(ex: Exception) -> ErrorDetails:
+    message = str(ex)
+    exc_info = sys.exc_info()
+    trace = traceback.format_exception(*exc_info)
+    return ErrorDetails(ex.__class__.__name__, message, "".join(trace))
 
 
 # e.g. log.error(*error_details(e))
-def error_details(ex: Exception) -> tuple[str, dict[str, Any]]:
-    msg = str(ex)
-    exc_info = sys.exc_info()
-    trace = traceback.format_exception(*exc_info)
-    details = {"type": ex.__class__.__name__, "message": msg, "stacktrace": "".join(trace)}
-    return msg, details
+def error_entry(ex: Exception) -> tuple[str, dict[str, Any]]:
+    details = error_details(ex)
+    return details.message, asdict(details)
--- tests/error.py
+++ tests/error.py
@@ -1,4 +1,4 @@
-from pyutils import error_details, log, stacktrace_details
+from pyutils import error_entry, log, stacktrace_entry, split_once
 
 
 def test_error_details():
@@ -6,7 +6,7 @@ def test_error_details():
     try:
         raise Exception("test error")
     except Exception as e:
-        log.error(*error_details(e))
+        log.error(*error_entry(e))
 
 
 def test_stacktrace_details():
@@ -14,4 +14,14 @@ def test_stacktrace_details():
     try:
         raise Exception("test error")
     except:
-        log.error(*stacktrace_details())
+        log.error(*stacktrace_entry())
+
+
+def test_split_once():
+    assert split_once("a:b:c") == ("a", "b:c")
+    assert split_once("a:b:c:d") == ("a", "b:c:d")
+    assert split_once("a:     b   ") == ("a", "b")
+    assert split_once("a") == ("", "a")
+    assert split_once("a:") == ("a", "")
+    assert split_once(":a") == ("", "a")
+    assert split_once("a#b#c", "#") == ("a", "b#c")
