20e5afa9f5e520bdb59c0af845a69ee3efc8b5a1
rwiv <zc798546@gmail.com>
2025-02-23 22:11:54 +0900

    fix: fix find_elem

--- pyutils/list.py
+++ pyutils/list.py
@@ -3,7 +3,7 @@ from typing import TypeVar, Callable
 T = TypeVar("T")
 
 
-def find_elem[T](lst: list, cond: Callable[[T], bool]) -> T | None:
+def find_elem(lst: list, cond: Callable[[T], bool]) -> T | None:
     for item in lst:
         if cond(item):
             return item
